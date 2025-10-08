d995ea6d5426d440790e776d3d5a8809e66bc43a
Speedforce24 <georgeemoefe@gmail.com>
2025-10-08 01:00:51 +0100

    UI

--- my-front/src/pages/Admin.js
+++ my-front/src/pages/Admin.js
@@ -187,7 +187,7 @@ useEffect(() => {
   <div className="h-screen w-full flex flex-col lg:flex-row items-center justify-center bg-gray-100">
     {/* Form Section - LEFT */}
     <div className="w-full lg:w-1/2 h-full overflow-y-auto flex items-center justify-center">
-      <div className="p-6 max-w-lg w-full h-full bg-white bg-opacity-80 rounded-lg shadow-lg m-4">
+      <div className="p-6 max-w-lg w-full h-full max-h-lg bg-white bg-opacity-80 rounded-lg shadow-lg m-4">
         <h2 className="text-2xl font-bold text-center mb-4">Admin Panel</h2>
 
         <form onSubmit={handleSubmit} className="space-y-4">
